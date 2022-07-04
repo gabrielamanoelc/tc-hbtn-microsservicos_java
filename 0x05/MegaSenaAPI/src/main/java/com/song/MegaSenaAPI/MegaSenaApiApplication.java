fe4c5f6d6055676032593c924bfb2e80a018a76e
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-07-04 14:23:31 -0700

    “g“

---
+++ 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaApiApplication.java
@@ -0,0 +1,5 @@
+package com.song.MegaSenaAPI; import org.springframework.boot.SpringApplication; import 
+org.springframework.boot.autoconfigure.SpringBootApplication; @SpringBootApplication public class MegaSenaApiApplication {
+	public static void main(String[] args) { SpringApplication.run(MegaSenaApiApplication.class, args);
+	}
+}
