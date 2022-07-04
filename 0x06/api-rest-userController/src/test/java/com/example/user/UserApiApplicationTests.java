c383fa0212826aa8b4a45da9c3b027dbc2be8d94
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-07-04 14:48:01 -0700

    “g“

---
+++ 0x06/api-rest-userController/src/test/java/com/example/user/UserApiApplicationTests.java
@@ -0,0 +1,5 @@
+package com.example.user; import org.junit.jupiter.api.Test; import org.springframework.boot.test.context.SpringBootTest; 
+@SpringBootTest class UserApiApplicationTests {
+	@Test void contextLoads() {
+	}
+}
