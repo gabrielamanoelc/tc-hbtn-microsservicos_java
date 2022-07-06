da5fb047c091d8eebf119b9e880cf3b3b044bd46
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-07-06 15:35:57 -0700

    “g“

---
+++ 0x08/ProductAPI/src/test/java/com/example/product/ProductApiApplicationTests.java
@@ -0,0 +1,5 @@
+package com.example.product; import org.junit.jupiter.api.Test; import org.springframework.boot.test.context.SpringBootTest; 
+@SpringBootTest class ProductApiApplicationTests {
+	@Test void contextLoads() {
+	}
+}
